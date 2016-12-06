5fe8d6f5460a0c0a54855fc6caae5ba5c70dee25
subash chebolu <subashch6@gmail.com>
2016-12-05 19:35:30 -0500

    starting project 2

---
+++ README.md
@@ -0,0 +1,1 @@
+Project 2 for class 5462 by Subahs Chebolu and Andrew Lee
---
+++ tcp1pr2.cc
@@ -0,0 +1,233 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+
+using namespace ns3;
+
+class MyApp : public Application
+{
+public:
+  MyApp ();
+  virtual ~MyApp ();
+
+  /**
+   * Register this type.
+   * \return The TypeId.
+   */
+  static TypeId GetTypeId (void);
+  void Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate);
+
+private:
+  virtual void StartApplication (void);
+  virtual void StopApplication (void);
+
+  void ScheduleTx (void);
+  void SendPacket (void);
+
+  Ptr<Socket>     m_socket;
+  Address         m_peer;
+  uint32_t        m_packetSize;
+  uint32_t        m_nPackets;
+  DataRate        m_dataRate;
+  EventId         m_sendEvent;
+  bool            m_running;
+  uint32_t        m_packetsSent;
+};
+
+MyApp::MyApp ()
+  : m_socket (0),
+    m_peer (),
+    m_packetSize (0),
+    m_nPackets (0),
+    m_dataRate (0),
+    m_sendEvent (),
+    m_running (false),
+    m_packetsSent (0)
+{
+}
+
+MyApp::~MyApp ()
+{
+  m_socket = 0;
+}
+
+/* static */
+TypeId MyApp::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("MyApp")
+    .SetParent<Application> ()
+    .SetGroupName ("Tutorial")
+    .AddConstructor<MyApp> ()
+    ;
+  return tid;
+}
+
+void
+MyApp::Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate)
+{
+  m_socket = socket;
+  m_peer = address;
+  m_packetSize = packetSize;
+  m_nPackets = nPackets;
+  m_dataRate = dataRate;
+}
+
+void
+MyApp::StartApplication (void)
+{
+  m_running = true;
+  m_packetsSent = 0;
+  m_socket->Bind ();
+  m_socket->Connect (m_peer);
+  SendPacket ();
+}
+
+void
+MyApp::StopApplication (void)
+{
+  m_running = false;
+
+  if (m_sendEvent.IsRunning ())
+    {
+      Simulator::Cancel (m_sendEvent);
+    }
+
+  if (m_socket)
+    {
+      m_socket->Close ();
+    }
+}
+
+void
+MyApp::SendPacket (void)
+{
+  Ptr<Packet> packet = Create<Packet> (m_packetSize);
+  m_socket->Send (packet);
+
+  if (++m_packetsSent < m_nPackets)
+    {
+      ScheduleTx ();
+    }
+}
+
+void
+MyApp::ScheduleTx (void)
+{
+  if (m_running)
+    {
+      Time tNext (Seconds (m_packetSize * 8 / static_cast<double> (m_dataRate.GetBitRate ())));
+      m_sendEvent = Simulator::Schedule (tNext, &MyApp::SendPacket, this);
+    }
+}
+
+static void
+CwndChange (Ptr<OutputStreamWrapper> stream, uint32_t oldCwnd, uint32_t newCwnd)
+{
+  NS_LOG_UNCOND (Simulator::Now ().GetSeconds () << "\t" << newCwnd);
+  *stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << newCwnd << std::endl;
+}
+/*
+static void
+RxDrop (Ptr<PcapFileWrapper> file, Ptr<const Packet> p)
+{
+  NS_LOG_UNCOND ("RxDrop at " << Simulator::Now ().GetSeconds ());
+  file->Write (Simulator::Now (), p);
+}
+*/
+
+int main (int argc, char *argv[]) {
+
+  	// Ensure Proper Argument Usage
+	if (argc != 5) {
+		printf("%d ERROR: Incorrect number of arguements.", argc);
+		printf(" Please run using the following format...\n");
+		printf("tcpchain <BER> <Data-Rate> <pcap-filename> <congestion-output>\n\n");
+		exit(1);
+    }
+	float BER = atof(argv[1]);
+	char *dataRate = argv[2];
+	char *pcapName = argv[3];
+	char *congest = argv[4];
+	
+	NS_LOG_COMPONENT_DEFINE ("TCPChain");
+
+  NodeContainer nodes;
+  nodes.Create (4);
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+  	// Set Devices
+	//NetDeviceContainer devices;
+	//devices = pointToPoint.Install (nodes);
+	NetDeviceContainer devAB, devBC, devCD;
+	devAB = pointToPoint.Install (nodes.Get(0), nodes.Get(1));
+	devBC = pointToPoint.Install (nodes.Get(1), nodes.Get(2));
+	devCD = pointToPoint.Install (nodes.Get(2), nodes.Get(3));
+
+	// Initialize Stack
+	InternetStackHelper stack;
+	stack.Install (nodes);
+
+	// Initialize and assign Address
+	NS_LOG_INFO ("CREATE: IP Address");
+	Ipv4AddressHelper address;
+	address.SetBase ("10.1.1.0", "255.255.255.0");
+	Ipv4InterfaceContainer interfaceAB = address.Assign (devAB);
+	address.SetBase ("11.1.1.0", "255.255.255.0");
+	Ipv4InterfaceContainer interfaceBC = address.Assign(devBC);
+	address.SetBase ("12.1.1.0", "255.255.255.0");
+	Ipv4InterfaceContainer interfaceCD = address.Assign(devCD);
+
+	// Set Bit Error Rate for A-B, C-D
+  Ptr<RateErrorModel> em = CreateObject<RateErrorModel> ();
+  em->SetAttribute ("ErrorRate", DoubleValue (0.000001));
+  devAB.Get (1)->SetAttribute ("ReceiveErrorModel", PointerValue (em));
+  devCD.Get (1)->SetAttribute ("ReceiveErrorModel", PointerValue (em));
+  
+    // Set Bit Error Rate for B-C
+  em->SetAttribute ("ErrorRate", DoubleValue (BER));
+  devBC.Get (1)->SetAttribute ("ReceiveErrorModel", PointerValue (em));
+
+  uint16_t sinkPort = 8080;
+  Address sinkAddress (InetSocketAddress (interfaceCD.GetAddress (1), sinkPort));
+  PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
+  ApplicationContainer sinkApps = packetSinkHelper.Install (nodes.Get (3));
+  sinkApps.Start (Seconds (0.));
+  sinkApps.Stop (Seconds (20.));
+
+  Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (nodes.Get (0), TcpSocketFactory::GetTypeId ());
+
+  //Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (nodes.Get (0), TcpSocketFactory::GetTypeId ());
+  //ns3TcpSocket->TraceConnectWithoutContext ("CongestionWindow", MakeBoundCallback (&CwndChange));
+
+  Ptr<MyApp> app = CreateObject<MyApp> ();
+  app->Setup (ns3TcpSocket, sinkAddress, 1040, 1000, DataRate (dataRate));
+  nodes.Get (0)->AddApplication (app);
+  app->SetStartTime (Seconds (1.));
+  app->SetStopTime (Seconds (20.));
+  
+  AsciiTraceHelper asciiTraceHelper;
+  Ptr<OutputStreamWrapper> stream = asciiTraceHelper.CreateFileStream (congest);
+  ns3TcpSocket->TraceConnectWithoutContext ("CongestionWindow", MakeBoundCallback (&CwndChange, stream));
+
+  //PcapHelper pcapHelper;
+  //Ptr<PcapFileWrapper> file = pcapHelper.CreateFile (pcapName, std::ios::out, PcapHelper::DLT_PPP);
+  //devAB.Get (1)->TraceConnectWithoutContext ("PhyRxDrop", MakeBoundCallback (&RxDrop, file));
+  //devBC.Get (1)->TraceConnectWithoutContext ("PhyRxDrop", MakeBoundCallback (&RxDrop, file));
+  //devCD.Get (1)->TraceConnectWithoutContext ("PhyRxDrop", MakeBoundCallback (&RxDrop, file));
+  
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+	pointToPoint.EnablePcapAll(pcapName);
+
+  Simulator::Stop (Seconds (20));
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+
+}
+
